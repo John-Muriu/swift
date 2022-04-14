53dc0e73960388c98390a0b7a4a3a3275054661e
john <johnmuriw@gmail.com>
2022-04-14 11:17:31 +0300

    first commit

---
+++ doczrc.js
@@ -0,0 +1,7 @@
+export default {
+    title: "SwifttDial Voice System Project Charter",
+    description: "call Center  Application  Upgrade",
+    ignore: ["README.md"],
+    dest: '/docs',
+    base: '/docs',
+};
